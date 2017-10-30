e8b7a0131dc2752dcef83aba3cb68c2b7d89cd6a
Lewis <munene2379@gmail.com>
2017-10-30 13:59:27 +0300

    crated the form in html and divided the colomun into two

---
+++ js/scripts.js
@@ -0,0 +1,50 @@
+
+var pingpong = function(ping) {
+  var numberArray = [];
+  if (ping < 0) {
+    for (var index = -1; index >= ping; index--) {
+      numberArray.push(index);
+    }
+    numberArray.forEach(function(ping) {
+      if (ping % 15 === 0) {
+        numberArray[-ping-1] = 'pingpong';
+      } else if (ping % 3 === 0) {
+        numberArray[-ping-1] = 'ping';
+      } else if (ping % 5 === 0) {
+        numberArray[-ping-1] = 'pong';
+      }
+    });
+  } else if (ping > 0) { //positive numbers
+      for (var index = 1; index <= ping; index++) {
+        numberArray.push(index);
+      }
+      numberArray.forEach(function(ping) {
+        if (ping % 15 === 0) {
+          numberArray[ping-1] = 'pingpong';
+        } else if (ping % 3 === 0) {
+          numberArray[ping-1] = 'ping';
+        } else if (ping % 5 === 0) {
+          numberArray[ping-1] = 'pong';
+        }
+      });
+  }
+  return numberArray;
+}
+
+var displayResultList = function(outputArray) {
+    $('ul#list').empty();
+  outputArray.forEach(function(item) {
+    $('ul#list').append('<li>' + item + '</li>');
+  });
+  return;
+}
+
+$(document).ready(function() {
+  $('form#pingpong').submit(function(event) {
+    var ping = $('input#ping').val();
+
+   displayResultList(pingpong(ping));
+
+   event.preventDefault();
+  });
+});
